fc85fd3ccfd7d31fb150cb639d83e58921d669df
wlippold <wlippold@hsartstech.org>
2016-12-22 07:21:22 -0500

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("play").onclick = function() { 
-     document.getElementById('demo').play();
-}
-
-document.getElementById("pause").onclick = function() { 
-     document.getElementById('demo').pause();
-}
-
